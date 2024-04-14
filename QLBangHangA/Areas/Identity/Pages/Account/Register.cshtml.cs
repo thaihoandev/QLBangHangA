4333f115c0a8dcbdc7a5f19dd0898e7d77af8fee
thaihoan2703 <hoanthai1103@gmail.com>
2024-04-14 12:43:04 +0700

    update package

--- QLBangHangA/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ QLBangHangA/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -145,6 +145,7 @@ namespace QLBangHangA.Areas.Identity.Pages.Account
                 var user = CreateUser();
 
                 user.FullName = Input.FullName;
+                user.Active = true;
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
